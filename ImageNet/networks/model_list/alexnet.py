7c2a838cbc31b78b8ac5f718c017cb4be4d7d7e1
jiecaoyu <jiecaoyu@umich.edu>
2017-09-17 02:52:16 -0400

    try original implmentation

--- ImageNet/networks/model_list/alexnet.py
+++ ImageNet/networks/model_list/alexnet.py
@@ -2,73 +2,83 @@ import torch
 import os
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
-
+import torch.nn.functional as F
 
 __all__ = ['AlexNet', 'alexnet']
 
-class LRN(nn.Module):
-    def __init__(self, local_size=1, alpha=1.0, beta=0.75, ACROSS_CHANNELS=True):
-        super(LRN, self).__init__()
-        self.ACROSS_CHANNELS = ACROSS_CHANNELS
-        if ACROSS_CHANNELS:
-            self.average=nn.AvgPool3d(kernel_size=(local_size, 1, 1),
-                    stride=1,
-                    padding=(int((local_size-1.0)/2), 0, 0))
-        else:
-            self.average=nn.AvgPool2d(kernel_size=local_size,
-                    stride=1,
-                    padding=int((local_size-1.0)/2))
-        self.alpha = alpha
-        self.beta = beta
+class BinActive(torch.autograd.Function):
+    '''
+    Binarize the input activations and calculate the mean across channel dimension.
+    '''
+    def forward(self, input):
+        self.save_for_backward(input)
+        size = input.size()
+        mean = torch.mean(input.abs(), 1, keepdim=True)
+        input = input.sign()
+        return input, mean
 
+    def backward(self, grad_output, grad_output_mean):
+        input, = self.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input.ge(1)] = 0
+        grad_input[input.le(-1)] = 0
+        return grad_input
 
+class BinConv2d(nn.Module):
+    def __init__(self, input_channels, output_channels,
+            kernel_size=-1, stride=-1, padding=-1, groups=1, dropout=0):
+        super(BinConv2d, self).__init__()
+        self.layer_type = 'BinConv2d'
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dropout_ratio = dropout
+
+        self.bn = nn.BatchNorm2d(input_channels, eps=1e-4, momentum=0.1, affine=False)
+        if dropout!=0:
+            self.dropout = nn.Dropout(dropout)
+        self.conv = nn.Conv2d(input_channels, output_channels,
+                kernel_size=kernel_size, stride=stride, padding=padding, groups=groups)
+        self.relu = nn.ReLU(inplace=True)
+    
     def forward(self, x):
-        if self.ACROSS_CHANNELS:
-            div = x.pow(2).unsqueeze(1)
-            div = self.average(div).squeeze(1)
-            div = div.mul(self.alpha).add(1.0).pow(self.beta)
-        else:
-            div = x.pow(2)
-            div = self.average(div)
-            div = div.mul(self.alpha).add(1.0).pow(self.beta)
-        x = x.div(div)
+        x = self.bn(x)
+        x, mean = BinActive()(x)
+        if self.dropout_ratio!=0:
+            x = self.dropout(x)
+        x = self.conv(x)
+        x = self.relu(x)
         return x
 
 class AlexNet(nn.Module):
 
     def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
+        self.num_classes = num_classes
         self.features = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            nn.BatchNorm2d(96, eps=1e-4, momentum=0.1, affine=False),
             nn.ReLU(inplace=True),
-            LRN(local_size=5, alpha=0.0001, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(96, 256, kernel_size=5, padding=2, groups=2),
-            nn.ReLU(inplace=True),
-            LRN(local_size=5, alpha=0.0001, beta=0.75),
+            BinConv2d(96, 256, kernel_size=5, stride=1, padding=2, groups=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(256, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 384, kernel_size=3, padding=1, groups=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1, groups=2),
-            nn.ReLU(inplace=True),
+            BinConv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            BinConv2d(384, 384, kernel_size=3, stride=1, padding=1, groups=2),
+            BinConv2d(384, 256, kernel_size=3, stride=1, padding=1, groups=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.classifier = nn.Sequential(
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
+            BinConv2d(256, 4096, kernel_size=6, stride=1, padding=0),
+            BinConv2d(4096, 4096, kernel_size=1, stride=1, padding=0, dropout=0.5),
+            nn.BatchNorm2d(4096, eps=1e-3, momentum=0.1, affine=False),
             nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
+            nn.Conv2d(4096, num_classes, kernel_size=1, stride=1, padding=0),
         )
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
         x = self.classifier(x)
+        x = x.view(x.size(0), self.num_classes)
         return x
 
 
