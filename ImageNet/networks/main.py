1300acbb4363ceaf2714d70ea44e87a18092c337
jiecaoyu <jiecaoyu@umich.edu>
2017-09-21 12:06:49 -0400

    change the number of required epoches

--- ImageNet/networks/main.py
+++ ImageNet/networks/main.py
@@ -34,7 +34,7 @@ parser.add_argument('--data', metavar='DATA_PATH', default='./data/',
                     help='path to imagenet data (default: ./data/)')
 parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
                     help='number of data loading workers (default: 8)')
-parser.add_argument('--epochs', default=200, type=int, metavar='N',
+parser.add_argument('--epochs', default=100, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
