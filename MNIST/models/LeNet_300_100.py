78ab55384a0b979462814215acc71c458a77aeb7
jiecaoyu <jiecaoyu@umich.edu>
2017-09-28 02:45:57 -0400

    commit for delete

--- MNIST/models/LeNet_300_100.py
+++ MNIST/models/LeNet_300_100.py
@@ -3,10 +3,64 @@ import torch
 import torch.nn as nn
 
 
+class BinActive(torch.autograd.Function):
+    '''
+    Binarize the input activations and calculate the mean across channel dimension.
+    '''
+    def forward(self, input):
+        self.save_for_backward(input)
+        size = input.size()
+        input = input.sign()
+        return input
+
+    def backward(self, grad_output):
+        input, = self.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input.ge(1)] = 0
+        grad_input[input.le(-1)] = 0
+        return grad_input
+
+class BinConv2d(nn.Module): # change the name of BinConv2d
+    def __init__(self, input_channels, output_channels,
+            kernel_size=-1, stride=-1, padding=-1, groups=1, dropout=0,
+            Linear=False):
+        super(BinConv2d, self).__init__()
+        self.layer_type = 'BinConv2d'
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dropout_ratio = dropout
+
+        if dropout!=0:
+            self.dropout = nn.Dropout(dropout)
+        self.Linear = Linear
+        if not self.Linear:
+            self.bn = nn.BatchNorm2d(input_channels, eps=1e-4, momentum=0.1, affine=True)
+            self.conv = nn.Conv2d(input_channels, output_channels,
+                    kernel_size=kernel_size, stride=stride, padding=padding, groups=groups)
+        else:
+            self.bn = nn.BatchNorm1d(input_channels, eps=1e-4, momentum=0.1, affine=True)
+            self.linear = nn.Linear(input_channels, output_channels)
+        self.relu = nn.ReLU(inplace=True)
+        return
+
+    def forward(self, x):
+        x = self.bn(x)
+        x = BinActive()(x)
+        if self.dropout_ratio!=0:
+            x = self.dropout(x)
+        if not self.Linear:
+            x = self.conv(x)
+        else:
+            x = self.linear(x)
+        x = self.relu(x)
+        return x
+
 class LeNet_300_100(nn.Module):
     def __init__(self):
         super(LeNet_300_100, self).__init__()
         self.ip1 = nn.Linear(28*28, 300)
+        self.bn_ip1 = nn.BatchNorm1d(300, eps=1e-4, momentum=0.1, affine=True)
         self.relu_ip1 = nn.ReLU(inplace=True)
         self.ip2 = nn.Linear(300, 100)
         self.relu_ip2 = nn.ReLU(inplace=True)
